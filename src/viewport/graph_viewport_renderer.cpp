729da6efeaad04a5d181c4d28bb62a7ceec51308
leon <13628189941lyl@gmail.com>
2026-06-16 15:30:56 +0800

    fix(phase-D): QStringLiteral 消除 shader init 重载二义性

--- src/viewport/graph_viewport_renderer.cpp
+++ src/viewport/graph_viewport_renderer.cpp
@@ -43,7 +43,8 @@ void GraphViewportRenderer::initGL() {
 
 void GraphViewportRenderer::setupShaders() {
     m_rainbowShader = std::make_unique<glk::GLSLShader>();
-    if (!m_rainbowShader->init(":/shaders/rainbow.vert", ":/shaders/rainbow.frag")) {
+    if (!m_rainbowShader->init(QStringLiteral(":/shaders/rainbow.vert"),
+                                QStringLiteral(":/shaders/rainbow.frag"))) {
         qFatal("GraphViewportRenderer: failed to load rainbow shader");
     }
 }
